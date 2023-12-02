461b00e76e46b9eb001ef88f067e8e1c2125fd09
Sniree <97450065+joon0017@users.noreply.github.com>
2023-12-02 16:12:03 +0900

    added readme

--- DESIGN/CameraWebServer/CameraWebServer.ino
+++ DESIGN/CameraWebServer/CameraWebServer.ino
@@ -35,8 +35,8 @@
 // ===========================
 // Enter your WiFi credentials
 // ===========================
-const char* ssid = "****";
-const char* password = "****";
+const char* ssid = "SJ";
+const char* password = "tjdwns1111";
 
 void startCameraServer();
 void setupLedFlash(int pin);
